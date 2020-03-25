4186180a813e2b59b8b44c88c3188c4014c201cb
everisnever@163.com <everisnever@163.com>
2020-03-25 11:55:42 +0800

    user

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,35 @@
+/* eslint-disable */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../components/Home'
-import Login from '../components/Login'
-
+import Login from "../components/Login"
+import Home from "../components/Home"
+import Welcome from "../components/Welcome"
+import Users from "../components/user/Users"
 Vue.use(VueRouter)
 
 const routes = [
+  {path: '/',redirect: '/login'},
+  {path: '/login',component: Login},
+  {path: '/home',component: Home,redirect:'/welcome',
+    children:[
+      {path: '/welcome',component: Welcome},
+      {path: '/users',component: Users},
+    ]},
 
-  {
-    path:'/',redirect:'/login'
-  },
-  {
-    path:'/login',component:Login
-  },
-  {
-    path:'/home',component:Home
-  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const index = new VueRouter({
   routes
 })
-//navigation guaid导航守卫
+//挂载导航守卫
 index.beforeEach((to , from , next) => {
   if (to.path === '/login'){
     return next()
@@ -30,6 +38,6 @@ index.beforeEach((to , from , next) => {
   if (!tokenStr){
     return next('/login')
   }
-  next()  //有则放行
+  next()
 })
 export default index
