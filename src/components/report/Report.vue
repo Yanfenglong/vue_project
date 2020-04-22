3c46793e7664e6a1e6249f0309c7a80792f4184c
everisnever@163.com <everisnever@163.com>
2020-04-22 09:24:21 +0800

    report

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -7,41 +7,54 @@
             <el-breadcrumb-item>数据报表</el-breadcrumb-item>
         </el-breadcrumb>
         <el-card>
-            <div  id="main" style="width: 600px;height:400px;"></div>
+            <div  id="main" style="width: 1000px;height:400px;"></div>
         </el-card>
     </div>
 </template>
 <script>
+    import  _ from  'lodash'
 import  echarts from  'echarts'
 export default{
     data(){
         return {
+            options: {
+                title: {
+                    text: '用户来源'
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross',
+                        label: {
+                            backgroundColor: '#E9EEF3'
+                        }
+                    }
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: [
+                    {
+                        boundaryGap: false
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value'
+                    }
+                ]
+            }
         }
     },
-    mounted(){
+    async mounted(){
         //生命周期函数Life cycle function
         var myChart = echarts.init(document.getElementById('main'));
-        // 指定图表的配置项和数据
-        var option = {
-            title: {
-                text: 'ECharts 入门示例'
-            },
-            tooltip: {},
-            legend: {
-                data:['销量']
-            },
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        };
-        // 使用刚指定的配置项和数据显示图表。
-        myChart.setOption(option);
+       const {data:res} = await this.$http.get('reports/type/1')
+        const  result = _.merge(res.data,this.options)
+        myChart.setOption(result);
     },
     methods:{
     }
